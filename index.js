d7324186c3b5365fa3ce3a8db0a92d22d9f18819
Ro Flo <iroflood@msn.com>
2019-01-31 21:37:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+var katzNumber = [];
 
+
+
+function takeANumber(katzNumber){
+
+  katzNumber.push(``);
+
+  return (`You are number ${katzNumber.length}`);
+
+}
+
+
+
+function nowServing(katzDeliLine){
+
+  let i = 0;
+
+  while (i < katzDeliLine.length) {
+
+    i++;
+
+  }
+
+  if (katzDeliLine.length === 0) {
+
+    return "There is nobody waiting to be served!";
+
+  }
+
+  else
+
+  return (`Currently serving ${katzDeliLine.shift()}.`);
+
+  }
+
+
+
+var line = []
+
+function currentLine(deliLine){
+
+  let i = 0;
+
+  while (i < deliLine.length) {
+
+    line.push(` `+ [i+1] + `. `  + deliLine[i])
+
+    i++;
+
+  }
+
+  if(deliLine.length === 0) {
+
+  return "The line is currently empty.";
+
+    }else
+
+    return (`The line is currently:` + line);
